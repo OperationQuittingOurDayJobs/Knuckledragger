6aa7251d14e59931b2327ce1c065b56cbec14c68
Chris <c.curteman@gmail.com>
2019-09-28 14:14:04 -0500

    updating database

--- knuckledragger/models.py
+++ knuckledragger/models.py
@@ -2,10 +2,13 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+# Don't forget to register models and commit changes to server!!!
+
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
 
+# SITE MODELS
 class Room(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
@@ -36,5 +39,27 @@ class Item(models.Model):
     def __str__(self):
         return self.name
 
+class Attributes(models.Model):
+    brawny = models.CharField(max_length = 16)
 
-# Don't forget to register models and commit changes to server!!!
+class Type(models.Model):
+    number = models.IntegerField(default = 0)
+
+class Races(models.Model):
+    Arrivad = models.CharField(max_length = 16)
+
+class Brawny(models.Model):
+    type = models.ForeignKey(Type, on_delete = models.CASCADE)
+
+class Character(models.Model):
+    race = models.ForeignKey(Races, on_delete = models.CASCADE)
+    attributes = models.ForeignKey(Attributes, on_delete = models.CASCADE)
+    #inventory
+    weight = models.IntegerField(default = 0)
+    age = models.IntegerField(default = 0)
+    hit_points = models.IntegerField(default = 0)
+    name = models.CharField(max_length = 16)
+    #connections
+    speed = models.IntegerField(default = 0)
+
+

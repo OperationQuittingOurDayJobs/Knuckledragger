36d970891ffb874cf0f269b7e596300fe284e384
Chris <c.curteman@gmail.com>
2019-09-28 10:55:44 -0500

    registering database model to admin

--- knuckledragger/models.py
+++ knuckledragger/models.py
@@ -14,14 +14,12 @@ class Room(models.Model):
     def __str__(self):
         return self.title
 
-
+# GAME MODELS
 class Status(models.Model):
     state = models.CharField(max_length = 16, default = 'normal')
 
-
 class Rarity(models.Model):
     quality = models.CharField(max_length = 16, default = 'mundane')
-
 
 class Item(models.Model):
     weight = models.FloatField(default = 0)
@@ -39,3 +37,4 @@ class Item(models.Model):
         return self.name
 
 
+# Don't forget to register models and commit changes to server!!!

43c3103c3817a666bf9fed822fc95b361a845b35
Chris <c.curteman@gmail.com>
2019-10-09 16:37:39 -0500

    rerouting pages

--- knuckledragger/views.py
+++ knuckledragger/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 import requests
 import os
 
 def index(request):
-    return render(request, 'knuckledragger/index.html')
+    return render(request, 'site/index.html')

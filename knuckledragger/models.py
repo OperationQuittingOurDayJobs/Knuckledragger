820defd81ead938949db53ea90e6debab99489d6
Chris <c.curteman@gmail.com>
2019-10-09 16:07:23 -0500

    testing character model

--- knuckledragger/models.py
+++ knuckledragger/models.py
@@ -2,3 +2,10 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+class Character(models.Model):
+    first_name = models.CharField(max_length=16)
+    last_name = models.CharField(max_length=16)
+    health = models.PositiveSmallIntegerField()
+    def __str__(self):
+        return self.first_name
+

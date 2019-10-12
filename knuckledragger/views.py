ef84856fdf1c24e3bac1bd63c85bee1a2eee701b
Chris <c.curteman@gmail.com>
2019-10-12 09:30:04 -0500

    updating codebase

--- knuckledragger/views.py
+++ knuckledragger/views.py
@@ -4,11 +4,16 @@ import requests
 import os
 
 from .models import Character
+# from .actions import Attack *
 
 def index(request):
     return render(request, 'site/index.html')
 
 def test(request):
-    character_list = Character.objects.order_by('id')
-    context = {'character_list':character_list }
+    character_list = Character.objects.filter(first_name='Test')
+
+    context = {'character_list' : character_list }
     return render(request, 'site/test/test.html', context)
+
+def attack(request):
+    print('You are attacking!')

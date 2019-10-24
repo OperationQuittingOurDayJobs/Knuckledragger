ab87cb8a9699c202c837d572e98a14708a8db501
Chris <c.curteman@gmail.com>
2019-10-23 20:02:36 -0500

    settings changes

--- config/settings.py
+++ config/settings.py
@@ -5,10 +5,6 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 

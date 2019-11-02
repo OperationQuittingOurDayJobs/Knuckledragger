0fce68d604c0d15ffa2ace40c79a0dc97fcb5993
Chris <c.curteman@gmail.com>
2019-11-02 17:27:42 -0500

    adding entries to allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.knuckledraggerrpg.com']
 
 # Application definition
 INSTALLED_APPS = [

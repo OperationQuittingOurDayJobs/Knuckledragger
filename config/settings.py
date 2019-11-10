bcd89c487d6e9b00ae493968940bf8e4a5a0f0ad
Chris <c.curteman@gmail.com>
2019-11-09 18:37:00 -0600

    Added AWS variables to settings with environment variables

--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -104,5 +105,9 @@ STATIC_URL = "/static/"
 
 LOGIN_REDIRECT_URL = 'lobby'
 LOGIN_URL = 'login'
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME') 
 
 django_heroku.settings(locals())
